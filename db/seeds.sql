b56cc78910b347807f95edc05a2a6a4f020241ce
indy6678 <indy6678@gmail.com>
2022-08-14 16:44:54 -0500

    added seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,28 @@
 INSERT INTO department (id, name)
 VALUES 
-    (1, 'Service'),
+    (1, 'Legal'),
     (2, 'Sales'),
     (3, 'Engineering'),
-    (4, 'Management');
+    (4, 'Finance');
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+    (1, 'Sales Lead', 10000),
+    (2, 'Salesperson', 80000),
+    (3, 'Lead Engineer', 150000),
+    (4, 'Software Engineer', 120000),
+    (5, 'Account Manager', 160000),
+    (6, 'Accountant', 125000),
+    (7, 'Legal Team Lead', 250000),
+    (8, 'Lawyer', 190000);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+    (1, 'John', 'Doe'),
+    (2, 'Mike', 'Chan'),
+    (3, 'Ashley', 'Rodriguez'),
+    (4, 'Kevin', 'Tupik'),
+    (5, 'Kunal', 'Singh'),
+    (6, 'Malia', 'Brown'),
+    (7, 'Sarah', 'Lourd'),
+    (8, 'Tom', 'Allen');

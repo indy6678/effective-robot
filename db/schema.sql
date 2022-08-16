d9f06493a5e0e726de62864cdd2c239fa7f29eaf
indy6678 <indy6678@gmail.com>
2022-08-15 23:23:23 -0500

    added foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
 CREATE TABLE department (
     id INT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
@@ -7,7 +11,8 @@ CREATE TABLE role (
     id INT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(jd) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -16,4 +21,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );

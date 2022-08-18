72cb4f8902a6f9c333e84eaaf4d7935402ddd881
indy6678 <indy6678@gmail.com>
2022-08-17 22:17:47 -0500

    added PUT routes

--- server.js
+++ server.js
@@ -63,43 +63,43 @@ app.get("/api/departments", (req, res) => {
 // });
 
 // route to show current employees
-app.get('/api/employees', (req, res) => {
-  const sql = `SELECT employee.first_name, employee.last_name, employee.first_name
+app.get("/api/employees", (req, res) => {
+  const sql = `SELECT employee.first_name, employee.last_name, role.title, role.department_id AS department, employee.manager_id
                 AS manager
                 FROM employee
                 LEFT JOIN role
                 ON employee.manager_id = employee.id`;
 
   db.query(sql, (err, rows) => {
-    if(err) {
-      res.status(500).json({error: err.message});
+    if (err) {
+      res.status(500).json({ error: err.message });
       return;
     }
     res.json({
-      message: 'success',
-      data: rows
+      message: "success",
+      data: rows,
     });
-    console.table(rows)
+    console.table(rows);
   });
 });
 
 // route to show current roles
-app.get('/api/roles', (req, res) => {
-  const sql = `SELECT role.*, department.name
+app.get("/api/roles", (req, res) => {
+  const sql = `SELECT role.title, role.salary, department.name
                AS department
                FROM role
                LEFT JOIN department
                ON role.department_id = department.id`;
 
   db.query(sql, (err, rows) => {
-    if(err) {
-      res.status(500).json({ error: err.message});
+    if (err) {
+      res.status(500).json({ error: err.message });
     }
     res.json({
-      message: 'success',
-      data: rows
+      message: "success",
+      data: rows,
     });
-    console.table(rows)
+    console.table(rows);
   });
 });
 
@@ -117,92 +117,108 @@ app.get("/api/department/:id", (req, res) => {
       message: "success",
       data: row,
     });
-    console.table(row)
+    console.table(row);
   });
 });
 
 // route to add a department
-app.post('/api/department', ({body}, res) => {
-    const sql = `INSERT INTO department (name)
+app.post("/api/department", ({ body }, res) => {
+  const sql = `INSERT INTO department (name)
     VALUES(?)`;
-    const params = [body.name];
+  const params = [body.name];
 
-    db.query(sql, params, (err, result) => {
-        if(err) {
-            res.status(400).json({error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
     });
+  });
 });
 
 // route to add an employee
-app.post('api/employee', ({body}, res) => {
-    const sql = `INSERT INTO employee (first_name, last_name, role, manager)
+app.post("api/employee", ({ body }, res) => {
+  const sql = `INSERT INTO employee (first_name, last_name, role, manager)
     VALUES(?,?,?,?)`;
-    const params = [body.first_name, body.last_name, body.role, body.manager];
+  const params = [body.first_name, body.last_name, body.role, body.manager];
 
-    db.query(sql, params, (err, results) => {
-      if (err) {
-        res.status(400).json({error: err.message});
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
-      });
+  db.query(sql, params, (err, results) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
     });
+  });
 });
 
 // route to add a role
-app.post('api/role', ({body}, res) => {
+app.post("api/role", ({ body }, res) => {
   const sql = `INSERT INTO role (name, name, department)
   VALUES(?,?,?)`;
   const params = [body.name, body.salary, body.department];
 
   db.query(sql, params, (err, results) => {
     if (err) {
-      res.status(400).json({error: err.message});
+      res.status(400).json({ error: err.message });
       return;
     }
     res.json({
-      message: 'success',
-      data: body
+      message: "success",
+      data: body,
     });
   });
 });
 
-// database query to delete a single row
-app.delete('/api/department/:id', (req, res) => {
+// database query to delete a single department
+app.delete("/api/department/:id", (req, res) => {
   const sql = `DELETE FROM department WHERE id = ?`;
-  const params = req.params.id;
-  db.query(sql, params, (err, row) => {
-    if(err) {
-        console.log(err);
-        res.status(400).json({error: err.message});
-        return;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
     } else if (!result.affectedRows) {
       // if the department was not found
       res.json({
-        message: "Department not found"
+        message: "Department not found",
       });
     } else {
       res.json({
-        message: 'Deleted', // confirmation message of a successful deletion
+        message: "Deleted", // confirmation message of a successful deletion
         changes: result.affectedRows, // shows how many rows were affected
-        id: req.params.id // shows the id of the affected row
-      })
+        id: req.params.id, // shows the id of the affected row
+      });
     }
-    console.log(result);
+  });
 });
-})
 
-// route to add
-// app.post()
-// query to add
+// database query to delete a single employee
+app.delete('/api/employee/:id', (req, res) => {
+  const sql = `DELETE FROM employee WHERE id = ?`;
+  const params = req.params.id;
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    } else if (!result.affectedRows) {
+      // if the employee was not found
+      res.json({
+        message: "Employee not found",
+      });
+    } else {
+      res.json({
+        message: "Deleted", // confirmation message of a successful deletion
+        changes: result.affectedRows, // shows how many rows were affected
+        id: req.params.id, // shows the id of the affected row
+      });
+    }
+  });
+});
 
 // response used for requests not accounted for; keep last
 app.use((req, res) => {
